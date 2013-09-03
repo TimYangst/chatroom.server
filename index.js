7967c56779d89851d272ef1bf168f08033edaff0
Ting Yang <t-tiyan@microsoft.com>
2013-09-03 17:23:20 +0800

    add recent binary of client side

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ console.log("Hello World");
 var server = require("./server");
 var router = require("./router");
 var requestHandlers = require("./requestHandler.js");
+var hapi = require('hapi');
+var config =  require("./config");
 
 var handle = {}
 handle["/"] = requestHandlers.messagebox;
@@ -23,6 +25,17 @@ handle["/post"] = requestHandlers.post;
 handle["/heartbeat"] = requestHandlers.heartbeat;
 handle["/messagebox"] = requestHandlers.messagebox;
 
-
-
 server.start(router.route,handle);
+
+var hapi_server = hapi.createServer(config.LOCAL_IP, config.UPDATE_PORT);
+
+hapi_server.route({
+    method : 'GET',
+    path : '/update/{path*}',
+    handler : {
+        directory: { path : './update/', listing : true, index: false }
+    }
+});
+
+hapi_server.start();
+console.log("hapi server is start at : "+ config.UPDATE_PORT);
